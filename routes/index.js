1e84879e6e25e560fabdd4f1645364f83e3484dc
Dhruv Patel <dhruv225@outlook.com>
2023-05-22 18:53:59 -0400

    Post and push form info into messages array

--- routes/index.js
+++ routes/index.js
@@ -19,5 +19,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: "Mini Messageboard", messages: messages })
 });
 
+router.get('/new', (req, res) => {
+  res.render('form')
+})
+
+router.post('/new', (req, res) => {
+  messages.push({text: req.body.messageText, user: req.body.name, added: new Date()})
+  res.redirect('/')
+})
+
 
 module.exports = router;
